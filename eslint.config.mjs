ce9dc50bd72a1ed3c61838b5da1477d86fbd5077
Sheraz Tariq <sheraztariq2978@gmail.com>
2024-04-19 14:15:39 +0500

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   pluginReactConfig,
 ];
